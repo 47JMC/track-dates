f3dc09d8eb42b72b8eecc6e9d60d5a9ab13993b9
47JMC <mjabir.5b@gmail.com>
2025-11-08 12:34:14 +0530

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,21 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const Nunito = localFont({
+const nunitoFont = localFont({
   src: "./fonts/Nunito-Regular.ttf",
-
+  weight: "400",
   variable: "--font-nunito",
+});
+const fredokaFont = localFont({
+  src: "./fonts/Fredoka-Regular.ttf",
+  weight: "400",
+  variable: "--font-fredoka",
+});
+
+const ubuntuMonoFont = localFont({
+  src: "./fonts/UbuntuMono-Regular.ttf",
+  weight: "400",
+  variable: "--font-ubuntu-mono",
 });
 
 export const metadata: Metadata = {
@@ -20,7 +31,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${Nunito.variable} antialiased`}>{children}</body>
+      <body
+        className={`${nunitoFont.variable} ${fredokaFont.variable} ${ubuntuMonoFont.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
