c2b41f9c262041d7016e835191b62bf8e58199e6
47JMC <mjabir.5b@gmail.com>
2025-11-09 10:50:47 +0530

    add days and hours

--- app/components/Tracker.tsx
+++ app/components/Tracker.tsx
@@ -11,6 +11,8 @@ function Tracker({ rDate }: Props) {
   // Implement tracking days left until GTA VI release
   // Nov 19, 2026
 
+  const [yearsLeft, setYearsLeft] = useState<number | null>(null);
+  const [monthsLeft, setMonthsLeft] = useState<number | null>(null);
   const [daysLeft, setDaysLeft] = useState<number | null>(null);
   const [hoursLeft, setHoursLeft] = useState<number | null>(null);
   const [minutesLeft, setMinutesLeft] = useState<number | null>(null);
@@ -21,14 +23,18 @@ function Tracker({ rDate }: Props) {
       const now = new Date();
       const timeDiff = releaseDate.getTime() - now.getTime();
 
+      const yearsLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24 * 365));
+      const monthsLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24 * 30));
       const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
       const hoursLeft = Math.ceil(timeDiff / (1000 * 60 * 60));
       const minutesLeft = Math.ceil(timeDiff / (1000 * 60));
 
+      setYearsLeft(yearsLeft);
+      setMonthsLeft(monthsLeft);
       setDaysLeft(daysLeft);
       setHoursLeft(hoursLeft);
       setMinutesLeft(minutesLeft);
-    }, 120); // Update every 24 hours
+    }, 120);
   });
 
   return (
@@ -37,6 +43,8 @@ function Tracker({ rDate }: Props) {
         TIME UNTIL GTA VI:
       </p>
       <div className="border-5 flex justify-center *:m-5 border-sky-700 rounded-xl m-2 p-3 ">
+        <DisplayBox title="YEARS LEFT">{yearsLeft}</DisplayBox>
+        <DisplayBox title="MONTHS LEFT">{monthsLeft}</DisplayBox>
         <DisplayBox title="DAYS LEFT">{daysLeft}</DisplayBox>
         <DisplayBox title="HOURS LEFT">{hoursLeft}</DisplayBox>
         <DisplayBox title="MINUTES LEFT">{minutesLeft}</DisplayBox>
